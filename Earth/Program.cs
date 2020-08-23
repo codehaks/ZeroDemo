b14a3b6c32ad0055697397dd9477e638d1978932
Hakim Ghods <ghods.hakim@hotmail.com>
2020-08-23 11:52:31 +0430

    Add project files.

---
+++ Earth/Program.cs
@@ -0,0 +1,20 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+
+namespace Earth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Earth!");
+            using var socket = new RequestSocket(">tcp://localhost:5500");
+
+            Console.WriteLine("Message : ");
+            var msg = Console.ReadLine();
+            socket.SendFrame(msg);
+
+        }
+    }
+}
---
+++ Mars/Program.cs
@@ -0,0 +1,20 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+
+namespace Mars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello From Mars!");
+            using var socket = new ResponseSocket("@tcp://localhost:5500");
+
+            var msg = socket.ReceiveFrameString();
+
+            Console.WriteLine($"From Earth : {msg}");            
+
+        }
+    }
+}

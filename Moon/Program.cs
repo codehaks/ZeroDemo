2bfa71217562958e8a8b6bfee33f857081fe80c3
Hakim <ghods.hakim@hotmail.com>
2024-02-25 19:28:18 +0330

    do added

--- Moon/Program.cs
+++ Moon/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using NetMQ;
+using NetMQ.Sockets;
+
 Console.WriteLine("Hello, World!");
+
+using (PushSocket pushSocket = new PushSocket())
+{
+    pushSocket.Connect("tcp://127.0.0.1:5556"); // Connect to the PULL socket
+
+    string msg = "";
+
+    do
+    {
+        Console.WriteLine("Message : ");
+        msg = Console.ReadLine();
+
+        pushSocket.SendFrame(msg);
+    } while (!string.IsNullOrEmpty(msg));
+
+}
--- SpaceShip/Worker.cs
+++ SpaceShip/Worker.cs
@@ -1,3 +1,6 @@
+using NetMQ;
+using NetMQ.Sockets;
+
 namespace SpaceShip;
 
 public class Worker : BackgroundService
@@ -11,13 +14,25 @@ public class Worker : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        using var pullSocket = new PullSocket();
+        pullSocket.Bind("tcp://127.0.0.1:5556"); // Bind to the PUSH socket
         while (!stoppingToken.IsCancellationRequested)
         {
-            if (_logger.IsEnabled(LogLevel.Information))
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            }
-            await Task.Delay(1000, stoppingToken);
+            string msg = pullSocket.ReceiveFrameString();
+
+            _logger.LogInformation("Worker running at: {time} -> {msg}", DateTimeOffset.Now, msg);
+            //EmailJobDto emailJob = JsonConvert.DeserializeObject<EmailJobDto>(serializedJob);
+
+            // Process the received email job as needed
         }
+
+        //while (!stoppingToken.IsCancellationRequested)
+        //{
+        //    if (_logger.IsEnabled(LogLevel.Information))
+        //    {
+        //        _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+        //    }
+        //    await Task.Delay(1000, stoppingToken);
+        //}
     }
 }
